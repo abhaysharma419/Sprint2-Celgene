42ff8e3604bd1c59abfb9abd1fc97d2038483ddf
abhaysharma419 <abhaysharmagk@gmail.com>
2020-12-02 22:28:58 +0530

    Op chemo changes and Potiental patinets config changes

--- Devlopment/Prescription Follow up/code/insert_m_pat_trk_cfgrn.sql
+++ Devlopment/Prescription Follow up/code/insert_m_pat_trk_cfgrn.sql
@@ -248,3 +248,131 @@ VALUES
   '2012411'
 );
 
+
+/* Potential Patients */
+
+INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2) 
+VALUES
+  (1100,'OP HN','Potential Patients','候補症例数','1st',NULL,'感受性_1L
+感受性_1L',NULL,'2012411','2012411'),
+  (1101,'OP HN','Potential Patients','候補症例数','2nd',NULL,'感受性_1L',NULL,'2012411','2012411'),
+  (1102,'OP HN','Potential Patients','候補症例数','3rd 以降',NULL,'抵抗性_3L＋',NULL,'2012411','2012411'),
+  (1103,'OP HN','Potential Patients','候補症例数','1st',NULL,'抵抗性_1L',NULL,'2012411','2012411'),
+  (1104,'OP HN','Potential Patients','候補症例数','背景不明',NULL,'不明',NULL,'2012411','2012411');
+
+ INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2) 
+VALUES
+  (1105,'YV','Potential Patients','候補症例数','TOTAL',NULL,'O+Y_3L+',NULL,'2000181','2000181'),
+  (1106,'YV','Potential Patients','候補症例数','TOTAL',NULL,'OP_1L',NULL,'2000181','2000181'),
+  (1107,'YV','Potential Patients','候補症例数','TOTAL',NULL,'不明',NULL,'2000181','2000181'),
+  (1108,'YV','Potential Patients','候補症例数','TOTAL',NULL,'O+Y_1L',NULL,'2000181','2000181'),
+  (1109,'YV','Potential Patients','候補症例数','TOTAL',NULL,'YV',NULL,'2000181','2000181'),
+  (1110,'YV','Potential Patients','候補症例数','TOTAL',NULL,'不明',NULL,'2000181','2000181'),
+  (1111,'YV','Potential Patients','候補症例数','TOTAL',NULL,'OP_Adjuvant',NULL,'2000181','2000181'),
+  (1112,'YV','Potential Patients','候補症例数','TOTAL',NULL,'OP_2L',NULL,'2000181','2000181'),
+  (1113,'YV','Potential Patients','候補症例数','TOTAL',NULL,'O+Y_2L＋',NULL,'2000181','2000181');
+
+INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2) 
+VALUES
+  (1128,'OP RCC','Potential Patients','候補症例数','背景不明',NULL,'不明',NULL,'52324723','52324723'),
+  (1129,'OP RCC','Potential Patients','候補症例数','1st',NULL,'O+Y_1L',NULL,'52324723','52324723'),
+  (1130,'OP RCC','Potential Patients','候補症例数','3rd 以降',NULL,'OP_3L+',NULL,'52324723','52324723'),
+  (1131,'OP RCC','Potential Patients','候補症例数','2nd',NULL,'OP_2L',NULL,'52324723','52324723');
+
+INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2) 
+VALUES
+  (1132,'OPYV MELA','Potential Patients','候補症例数','Other',NULL,'OP_3L+',NULL,'2011827','2011827'),
+  (1133,'OPYV MELA','Potential Patients','候補症例数','Other',NULL,'OP_3L+',NULL,'2011827','2011827'),
+  (1134,'OPYV MELA','Potential Patients','候補症例数','2nd',NULL,'O+Y_2L＋',NULL,'2011827','2011827'),
+  (1135,'OPYV MELA','Potential Patients','候補症例数','2nd',NULL,'OP_2L',NULL,'2011827','2011827'),
+  (1135,'OPYV MELA','Potential Patients','候補症例数','1st',NULL,'O+Y_1L',NULL,'2011827','2011827'),
+  (1136,'OPYV MELA','Potential Patients','候補症例数','Other',NULL,'O+Y_3L+',NULL,'2011827','2011827'),
+  (1137,'OPYV MELA','Potential Patients','候補症例数','Other',NULL,'YV',NULL,'2011827','2011827'),
+  (1138,'OPYV MELA','Potential Patients','候補症例数','Other',NULL,'不明',NULL,'2011827','2011827'),
+  (1139,'OPYV MELA','Potential Patients','候補症例数','Adjuvant',NULL,'OP_Adjuvant',NULL,'2011827','2011827'),
+  (1140,'OPYV MELA','Potential Patients','候補症例数','1st',NULL,'OP_1L',NULL,'2011827','2011827');
+
+INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2) 
+VALUES
+  (1141,'OPYV RCC','Potential Patients','候補症例数','2nd',NULL,'O+Y_2L',NULL,'2012012','2012012'),
+  (1142,'OPYV RCC','Potential Patients','候補症例数','背景不明',NULL,'不明',NULL,'2012012','2012012'),
+  (1143,'OPYV RCC','Potential Patients','候補症例数','3rd 以降',NULL,'OP_3L＋',NULL,'2012012','2012012'),
+  (1144,'OPYV RCC','Potential Patients','候補症例数','1st',NULL,'O+Y_1L',NULL,'2012012','2012012');
+
+INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2)
+VALUES
+  (1115,'OP LC','Potential Patients','候補症例数','MPM',NULL,'MPM',NULL,'2009032','2009032'),
+  (1116,'OP LC','Potential Patients','候補症例数','背景不明',NULL,'不明',NULL,'2009032','2009032'),
+  (1117,'OP LC','Potential Patients','候補症例数','3rd 以降',NULL,'3L+',NULL,'2009032','2009032'),
+  (1118,'OP LC','Potential Patients','候補症例数','2nd',NULL,'2L',NULL,'2009032','2009032');
+ 
+ 
+  INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2)
+VALUES
+  (1119,'OP MELA','Potential Patients','候補症例数','Other',NULL,'不明',NULL,'52324719','52324719'),
+  (1120,'OP MELA','Potential Patients','候補症例数','Other',NULL,'YV',NULL,'52324719','52324719'),
+  (1121,'OP MELA','Potential Patients','候補症例数','Other',NULL,'O+Y_3L+',NULL,'52324719','52324719'),
+  (1122,'OP MELA','Potential Patients','候補症例数','Other',NULL,'OP_3L+',NULL,'52324719','52324719'),
+  (1123,'OP MELA','Potential Patients','候補症例数','2nd',NULL,'OP_2L',NULL,'52324719','52324719'),
+  (1124,'OP MELA','Potential Patients','候補症例数','2nd',NULL,'O+Y_2L＋',NULL,'52324719','52324719'),
+  (1125,'OP MELA','Potential Patients','候補症例数','1st',NULL,'O+Y_1L',NULL,'52324719','52324719'),
+  (1126,'OP MELA','Potential Patients','候補症例数','1st',NULL,'OP_1L',NULL,'52324719','52324719'),
+  (1127,'OP MELA','Potential Patients','候補症例数','Adjuvant',NULL,'OP_Adjuvant',NULL,'52324719','52324719');
+
+INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2)
+VALUES
+  (1146,'OP MELA','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'52324719','52324719'),
+  (1147,'OPYV RCC','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'2012012','2012012'),
+  (1148,'OP LC','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'2009032','2009032'),
+  (1149,'OP RCC','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'52324723','52324723'),
+  (1150,'OPYV MELA','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'2011827','2011827'),
+  (1151,'YV','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'2000181','2000181'),
+  (1152,'OP HN','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'2012411','2012411');
+
+
+INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2)
+VALUES
+  (1153,'OP MELA','New Patients','新規患者数',NULL,NULL,NULL,NULL,'52324719','52324719'),
+  (1154,'OPYV RCC','New Patients','新規患者数',NULL,NULL,NULL,NULL,'2012012','2012012'),
+  (1155,'OP LC','New Patients','新規患者数',NULL,NULL,NULL,NULL,'2009032','2009032'),
+  (1156,'OP RCC','New Patients','新規患者数',NULL,NULL,NULL,NULL,'52324723','52324723'),
+  (1157,'OPYV MELA','New Patients','新規患者数',NULL,NULL,NULL,NULL,'2011827','2011827'),
+  (1158,'YV','New Patients','新規患者数',NULL,NULL,NULL,NULL,'2000181','2000181'),
+  (1159,'OP HN','New Patients','新規患者数',NULL,NULL,NULL,NULL,'2012411','2012411');
+
+  INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2) 
+VALUES
+  (1160,'OP Chemo LC','Potential Patients','候補症例数','2nd',NULL,'2L',NULL,'2016229','2016229'),
+  (1161,'OP Chemo LC','Potential Patients','候補症例数','MPM',NULL,'MPM',NULL,'2016229','2016229'),
+  (1162,'OP Chemo LC','Potential Patients','候補症例数','1st',NULL,'1L',NULL,'2016229','2016229'),
+  (1163,'OP Chemo LC','Potential Patients','候補症例数','背景不明',NULL,'不明',NULL,'2016229','2016229'),
+  (1164,'OP Chemo LC','Potential Patients','候補症例数','3rd 以降',NULL,'3L+',NULL,'2016229','2016229'),
+  (1165,'OP Chemo LC','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'2016229','2016229');
+
+  INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2) 
+VALUES
+  (1166,'OPYV Chemo LC','Potential Patients','候補症例数','1st',NULL,'1L',NULL,'2016231','2016231'),
+  (1167,'OPYV Chemo LC','Potential Patients','候補症例数','背景不明',NULL,'不明',NULL,'2016231','2016231'),
+  (1168,'OPYV Chemo LC','Potential Patients','候補症例数','3rd 以降',NULL,'3L+',NULL,'2016231','2016231'),
+  (1169,'OPYV Chemo LC','Potential Patients','候補症例数','MPM',NULL,'MPM',NULL,'2016231','2016231'),
+  (1170,'OPYV Chemo LC','Potential Patients','候補症例数','2nd',NULL,'2L',NULL,'2016231','2016231'),
+  (1171,'OPYV Chemo LC','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'2016231','2016231');
+
+  INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2) 
+VALUES
+  (1172,'OPYV LC','Potential Patients','候補症例数','3rd 以降',NULL,'3L+',NULL,'2016230','2016230'),
+  (1173,'OPYV LC','Potential Patients','候補症例数','1st',NULL,'1L',NULL,'2016230','2016230'),
+  (1174,'OPYV LC','Potential Patients','候補症例数','背景不明',NULL,'不明',NULL,'2016230','2016230'),
+  (1175,'OPYV LC','Potential Patients','候補症例数','2nd',NULL,'2L',NULL,'2016230','2016230'),
+  (1176,'OPYV LC','Potential Patients','候補症例数','MPM',NULL,'MPM',NULL,'2016230','2016230'),
+  (1177,'OPYV LC','Potential Patients','候補症例数',NULL,NULL,NULL,NULL,'2016230','2016230');
+
+
+  INSERT INTO jp_ops.m_pat_trk_cfgrn (serl_num,prd_shrt_ds,pat_type_ds,pat_type_lcl_lang_ds,trtt_ln_txt,extl_id_vod__c,qstn_txt,ord_num,cpm_id,cpm_id_2)
+VALUES
+  (1178,'OP Chemo LC','New Patients','新規患者数',NULL,NULL,NULL,NULL,'2016229','2016229'),
+  (1179,'OPYV Chemo LC','New Patients','新規患者数',NULL,NULL,NULL,NULL,'2016231','2016231'),
+  (1180,'OPYV LC','New Patients','新規患者数',NULL,NULL,NULL,NULL,'2016230','2016230');
+
+
+
